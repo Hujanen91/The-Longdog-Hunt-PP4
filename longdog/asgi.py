a4b89b1cad50a41b3056a4f82e391a67ab4a21d1
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-02-27 10:11:35 +0000

    Correct errors in code after name change

--- longdog/asgi.py
+++ longdog/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for longdog_hunt project.
+ASGI config for longdog project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'longdog_hunt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'longdog.settings')
 
 application = get_asgi_application()
