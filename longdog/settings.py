860d2f7ca6c255cfd26524d30bee0eb58dd81419
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-02-29 22:23:33 +0000

    Install and create class for summernote

--- longdog/settings.py
+++ longdog/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-hujanen91-thelongdoghun-pdyykwod2mm.ws-eu108.gitpod.io','.herokuapp.com']
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
 ]
 
