3645ae4227de54ab7da92de742cb21b9d59d65f4
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-03-19 10:45:21 +0000

    Testing cloudinary static storage

--- longdog/settings.py
+++ longdog/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ '.gitpod.io',
-                 '.herokuapp.com']
+ALLOWED_HOSTS = ['https://the-longdog-hunt-9f116eb214b4.herokuapp.com', '127.0.0.1', '.gitpod.io']
 
 
 # Application definition
@@ -153,15 +152,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = (
     "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
 )
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 # Default primary key field type
