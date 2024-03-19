f7e8855eacf4ad3a64d457536084e5ec1c96770e
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-03-19 10:54:11 +0000

    Try settings to get rid of bad request heroku

--- longdog/settings.py
+++ longdog/settings.py
@@ -28,9 +28,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://the-longdog-hunt-9f116eb214b4.herokuapp.com', '127.0.0.1', '.gitpod.io']
+ALLOWED_HOSTS = ['https://the-longdog-hunt-9f116eb214b4.herokuapp.com',
+                 '127.0.0.1',
+                 '.gitpod.io',
+                 'https://the-longdog-hunt-9f116eb214b4.herokuapp.com/']
 
 
 # Application definition
