5acd147bc27d60b0b28f39a64ea4a91923d5fbf2
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-03-28 10:43:26 +0000

    Style commentfield width and padding

--- likes/admin.py
+++ likes/admin.py
@@ -2,8 +2,3 @@ from django.contrib import admin
 from .models import Like
 
 # Register your models here.
-
-@admin.register(Like)
-class LikeAdmin(admin.ModelAdmin):
-    list_display = ('user', 'post', 'created_at')
-    list_filter = ('created_at',)
