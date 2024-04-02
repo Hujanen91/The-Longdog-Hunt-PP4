93008256a0770aa49bd0f06ce336b024b10b7e22
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2024-04-02 13:59:50 +0000

    Add styling to liked_posts container

--- likes/views.py
+++ likes/views.py
@@ -10,4 +10,3 @@ def user_likes(request):
     # Retrieve liked posts associated with the current user
     liked_posts = request.user.liked_posts.all()
     return render(request, "liked_posts.html", {"liked_posts": liked_posts})
-    
